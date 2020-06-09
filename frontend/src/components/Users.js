048ab13b09e208cae3072de0a91e7b705a36e285
Victor Obrist <victor.obrist@gmail.com>
2020-06-09 12:30:30 +0200

    Frontend CRUD complete

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,27 +1,95 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const API = process.env.REACT_APP_API
+var fetch = require('node-fetch') // require for use fetch in nodejs
 
 export const Users = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const [id, setId] = useState('')
+  const [editing, setEditing] = useState(false)
+
+  const [users, setUsers] = useState([])
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const res = await fetch(`${API}/users`, {
-      method: 'POST',
-      header: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password
+    // console.log(API)
+    if (!editing) {
+      const response = await fetch(`${API}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
       })
-    })
-    const data = await res.json()
-    console.log(data)
+      const data = await response.json()
+      console.log(data)
+    } else {
+      const response = await fetch(`${API}/users/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
+      })
+      const data = await response.json()
+      console.log(data)
+      setId('')
+      setEditing(false)
+    }
+
+    await getUsers()
+
+    setName('')
+    setEmail('')
+    setPassword('')
+  }
+
+  const getUsers = async () => {
+    const response = await fetch(`${API}/users`)
+    const data = await response.json()
+    setUsers(data)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const editUser = async (id) => {
+    const response = await fetch(`${API}/users/${id}`)
+    const data = await response.json()
+
+    setId(id)
+    setEditing(true)
+
+    setName(data.name)
+    setEmail(data.email)
+    setPassword(data.password)
+
+    // console.log(data)
+    await getUsers()
+  }
+
+  const deleteUser = async (id) => {
+    const userResponse = window.confirm('Are you sure you want to delete it?')
+    if (userResponse) {
+      const response = await fetch(`${API}/users/${id}`, {
+        method: 'DELETE'
+      })
+      const data = await response.json()
+      console.log(data)
+      await getUsers()
+    }
   }
 
   return (
@@ -57,11 +125,45 @@ export const Users = () => {
             />
           </div>
           <button className='btn btn-primary btn-block'>
-            Create
+            {editing ? 'Update' : 'Create'}
           </button>
         </form>
       </div>
-      <div className='col-md-8' />
+      <div className='col-md-8'>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+                <td>
+                  <button
+                    className='btn btn-secondary btn-sm btn-block'
+                    onClick={(e) => editUser(user._id)}
+                  >
+                  Edit
+                  </button>
+                  <button
+                    className='btn btn-danger btn-sm btn-block'
+                    onClick={(e) => deleteUser(user._id)}
+                  >
+                  Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
